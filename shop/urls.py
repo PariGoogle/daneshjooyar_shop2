001791adff83415b787073cc5e70b2e949c5956a
pari <abazari.1369@gmail.com>
2024-07-07 10:39:56 +0430

    add project

--- shop/urls.py
+++ shop/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from shop.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name="index")
